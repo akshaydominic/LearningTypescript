60705e9349c72f36f27ac81b69a53bbbee21f54a
Akshay A <adominic21@gmail.com>
2021-05-01 16:06:45 +0530

    learing loops and array

---
+++ 1-helloworld/forloop_array.js
@@ -0,0 +1,12 @@
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+var Sports = ["baseball", "Cricket", "basketball", "Football"];
+for (var _i = 0, Sports_1 = Sports; _i < Sports_1.length; _i++) {
+    var Sports_team = Sports_1[_i];
+    console.log(Sports_team);
+}
+var num_array = [1, 10.8, 40, 80, 1000];
+for (var i = 0; i < num_array.length; i++) {
+    console.log(num_array[i]);
+}
---
+++ 1-helloworld/forloop_array.ts
@@ -0,0 +1,13 @@
+for(let i=0;i<5;i++){
+   console.log(i);
+}
+
+let Sports:string[] =["baseball","Cricket","basketball","Football"];
+for(let Sports_team of Sports){
+    console.log(Sports_team);
+}
+
+let num_array:number[] =[1,10.8,40,80,1000];
+for(let i=0;i<num_array.length;i++){
+    console.log(num_array[i]);
+}
