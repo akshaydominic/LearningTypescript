742f71cd2ddd044506b89669d3d7dada8712937f
Akshay A <adominic21@gmail.com>
2021-05-01 15:47:45 +0530

    learning variables

---
+++ 1-helloworld/variables.js
@@ -0,0 +1,9 @@
+var first_name = "Akshay";
+var last_name = "A";
+var Age = 23;
+var ismale = true;
+var doescode = true;
+console.log("First Name :" + first_name);
+console.log("Last Name :" + last_name);
+console.log("Age :" + Age);
+console.log("Male :" + ismale);
---
+++ 1-helloworld/variables.ts
@@ -0,0 +1,14 @@
+let first_name:string = "Akshay";
+let last_name:string = "A";
+let Age:number =23;
+let ismale: boolean = true;
+let doescode :any =true;
+
+console.log("First Name :"+first_name);
+console.log("Last Name :"+last_name);
+console.log("Age :"+Age);
+console.log("Male :"+ismale);
+
+
+
+
