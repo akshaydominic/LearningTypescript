fa6bae946d56638d871b2ef8e51baa0bf6998cbc
Akshay A <adominic21@gmail.com>
2021-05-01 22:42:21 +0530

    Parameter properties

---
+++ 2-classes/Employee.js
@@ -0,0 +1,12 @@
+"use strict";
+var Employee = /** @class */ (function () {
+    function Employee(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    return Employee;
+}());
+var akash = new Employee("akash", "t");
+console.log(akash);
---
+++ 2-classes/Employee.ts
@@ -0,0 +1,9 @@
+class Employee{
+    constructor(private firstName:string, private lastName:string){
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
+
+}
+let akash  = new Employee("akash","t");
+console.log(akash);
